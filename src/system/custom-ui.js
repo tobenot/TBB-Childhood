b3cb3c819ba07a8e62717c7d5730f4e7a86381c1
tobenot <2818373670@qq.com>
2025-05-04 11:21:51 +0800

    pref: improve ch1

--- src/system/custom-ui.js
+++ src/system/custom-ui.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     // 修改默认UI按钮为中文
     $('#menu-item-back a').text('后退');
     $('#menu-item-forward a').text('前进');
+    $('#menu-item-settings a').text('设置');
     $('#menu-item-author a').text('作者主页');
     $('#menu-item-saves a').text('存档 / 读档');
     $('#menu-item-restart a').text('重新开始');
